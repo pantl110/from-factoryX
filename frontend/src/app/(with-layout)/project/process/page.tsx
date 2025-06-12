27d95128ff39ecaf1494ce4e4a383c9bff7b39a8
june <june@teamwise.im>
2025-06-13 00:11:23 +0900

    design: project/completed page publishing CON-210

--- frontend/src/app/(with-layout)/project/process/page.tsx
+++ frontend/src/app/(with-layout)/project/process/page.tsx
@@ -2,9 +2,9 @@
 
 import { useRouter } from "next/navigation";
 import MainTitleSec from "@/app/(with-layout)/project/process/main-title-sec";
-import SearchDeleteTable from "@/app/(with-layout)/project/process/search-delete-table";
-import TableHeader from "@/app/(with-layout)/project/process/table-header";
-import TableItem from "@/app/(with-layout)/project/process/table-item";
+import SearchDeleteTable from "@/app/(with-layout)/project/search-delete-table";
+import TableHeader from "@/app/(with-layout)/project/table-header";
+import TableItem from "@/app/(with-layout)/project/table-item";
 
 const ProcessProjectPage = () => {
   const router = useRouter();
@@ -19,11 +19,11 @@ const ProcessProjectPage = () => {
       <div className="px-8">
         <SearchDeleteTable />
         <div>
-          <TableHeader />
+          <TableHeader lastLabel="납기일자" />
           {[...Array(9)].map((_, index) => (
             <TableItem
               key={index}
-              status="생산중"
+              status="생산 중"
               companyName="플라스틱이 좋아"
               items="플라스틱 컵 외 3개"
               startDate="2025-06-04"
--- frontend/src/app/(with-layout)/project/table-item.tsx
+++ frontend/src/app/(with-layout)/project/table-item.tsx
@@ -1,7 +1,12 @@
 import Chip from "@/ui/chip";
+import {
+  StatusType,
+  StatusColorType,
+  statusColorMap,
+} from "@/app/(with-layout)/project/statusColorMap";
 
 interface TableItemProps {
-  status: string;
+  status: StatusType;
   companyName: string;
   items: string;
   startDate: string;
@@ -15,13 +20,19 @@ const TableItem = ({
   startDate,
   endDate,
 }: TableItemProps) => {
+  const chipColors: StatusColorType = statusColorMap[status];
+
   return (
     <div className="flex items-center h-14 border-b border-[#eeeeee] Me_Body-1">
       <div className="flex items-center py-3 px-2">
         <input type="checkbox" className="w-4 h-4 border-sv" />
       </div>
       <div className="py-1 px-3 w-[150px]">
-        <Chip text={status} bgColor="bg-purple-8" textColor="text-purple" />
+        <Chip
+          text={status}
+          bgColor={chipColors.bgColor}
+          textColor={chipColors.textColor}
+        />
       </div>
       <p className="flex-1 py-1 px-3 text-dg">{companyName}</p>
       <p className="flex-1 py-1 px-3 text-dg">{items}</p>
