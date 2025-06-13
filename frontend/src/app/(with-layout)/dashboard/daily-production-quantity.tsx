0ba884dbc0e772c60fa8cbd00363868d062c86fc
dean-teamwise <dean@teamwise.im>
2025-06-13 11:48:59 +0900

    design: 부족한 품목 수 컴포넌트 구현

--- frontend/src/app/(with-layout)/dashboard/daily-production-quantity.tsx
+++ frontend/src/app/(with-layout)/dashboard/daily-production-quantity.tsx
@@ -4,7 +4,7 @@ import { Triangle } from "@phosphor-icons/react/dist/ssr";
 
 const DailyProductionQuantity = () => {
   return (
-    <div className="flex-1 pt-5 pb-4 px-5 rounded-lg border border-[#eeeeee]">
+    <div className="pt-5 pb-4 px-5 rounded-lg border border-[#eeeeee] h-[141px]">
       <div className="flex flex-col gap-1">
         <p className="Heading-4 text-sv">오늘 생산량</p>
         <p className="flex gap-1 Heading-1">
