0eb20b492be1b3e76f9d0e7c6e8138b35577ec24
dean-teamwise <dean@teamwise.im>
2025-06-17 10:55:29 +0900

    design: 프로덕션-납품 페이지 간격 수정

--- frontend/src/app/(with-layout)/production/delivery/delivery-table-header.tsx
+++ frontend/src/app/(with-layout)/production/delivery/delivery-table-header.tsx
@@ -7,12 +7,12 @@ const DeliveryTableHeader = () => {
       <p className="flex-1 py-1 px-3 text-sv">품목코드</p>
       <p className="flex-[2] py-1 px-3 text-sv">품목명</p>
       <p className="flex-1 py-1 px-3 text-sv">규격</p>
-      <p className="flex-[80px] py-1 px-3 text-sv">단위</p>
+      <p className="w-[80px] py-1 px-3 text-sv">단위</p>
       <p className="flex-1 py-1 px-3 text-sv">납품수량</p>
-      <p className="flex-[100px] py-1 px-3 text-sv">단가</p>
+      <p className="w-[100px] py-1 px-3 text-sv">단가</p>
       <p className="flex-1 py-1 px-3 text-sv">금액</p>
       <p className="flex-1 py-1 px-3 text-sv">납품일자</p>
-      <p className="flex-[130px] py-1 px-3 text-sv">납품표</p>
+      <p className="w-[130px] py-1 px-3 text-sv">납품표</p>
     </div>
   );
 };
