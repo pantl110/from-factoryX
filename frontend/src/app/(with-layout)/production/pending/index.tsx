b051be5bc9d3070f3f0eb9f94084bb26e2862fb8
dean-teamwise <dean@teamwise.im>
2025-06-17 13:27:26 +0900

    design: 프로덕션 생산 대기 페이지 퍼블리싱

--- frontend/src/app/(with-layout)/production/pending/index.tsx
+++ frontend/src/app/(with-layout)/production/pending/index.tsx
@@ -1,5 +1,25 @@
+import ProductFlowTitle from "./product-flow-title";
+import PendingTableHeader from "./pending-table-header";
+import PendingTableItem from "./pending-table-item";
+
 const PendingPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <ProductFlowTitle />
+      <div className="px-10 pt-5 pb-9">
+        <PendingTableHeader />
+        <PendingTableItem />
+        <PendingTableItem />
+        <PendingTableItem />
+        <PendingTableItem />
+        <PendingTableItem />
+        <PendingTableItem />
+        <PendingTableItem />
+        <PendingTableItem />
+        <PendingTableItem />
+      </div>
+    </>
+  );
 };
 
 export default PendingPage;
