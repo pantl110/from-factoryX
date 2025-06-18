b09801bbccdf9a401b4cae25deb6d034d1d2ab9a
june <june@teamwise.im>
2025-06-18 15:20:56 +0900

    tax 페이지 패널 나올 시 뒤에 스크롤 막기, 밖에 클릭하면 나가기

--- frontend/src/app/(with-layout)/setting/master-data/client/client-table/client-table-header.tsx
+++ frontend/src/app/(with-layout)/setting/master-data/client/client-table/client-table-header.tsx
@@ -1,6 +1,6 @@
 const ClientTableHeader = () => {
   return (
-    <div className="flex h-12 items-center py-1 px-3 w-full border-t border-b border-[#eeeeee] Me_Body-1 text-sv">
+    <div className="flex h-12 w-[1697px] items-center py-1 px-3 border-t border-b border-[#eeeeee] Me_Body-1 text-sv">
       <p className="flex-1">회사명</p>
       <p className="flex-1">사업자등록번호</p>
       <p className="w-[100px]">대표자명</p>
--- frontend/src/app/(with-layout)/tax/page.tsx
+++ frontend/src/app/(with-layout)/tax/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TaxDetailPanel from "./tax-detail-panel";
 import MainTitleSec from "./main-title-sec";
 import SearchDeleteTable from "./search-delete-table";
@@ -22,7 +22,9 @@ const TaxPage = () => {
 
   return (
     <>
-      <div className="flex flex-col gap-8">
+      <div
+        className={`flex flex-col gap-8 ${isPanelOpen ? "overflow-hidden" : ""}`}
+      >
         <MainTitleSec />
         <div className="px-8">
           <SearchDeleteTable />
@@ -46,7 +48,10 @@ const TaxPage = () => {
         </div>
       </div>
       {isPanelOpen && (
-        <div className="fixed inset-0 bg-bl/50 transition-opacity duration-300" />
+        <div
+          className="fixed inset-0 bg-bl/50 transition-opacity duration-300"
+          onClick={handleClosePanel}
+        />
       )}
       <div
         className={`fixed top-0 right-0 h-full transition-transform duration-300 ease-in-out ${
