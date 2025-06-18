64915f1b4164b30bc3b5c5ca0d4a51e1ee9e4b77
june <june@teamwise.im>
2025-06-18 13:56:18 +0900

    fix: id props 추가

--- frontend/src/app/(with-layout)/document/page.tsx
+++ frontend/src/app/(with-layout)/document/page.tsx
@@ -15,7 +15,7 @@ const DocumentPage = () => {
 
   return (
     <>
-      <div className="flex flex-col gap-2 w-full">
+      <div className="flex flex-col gap-8 w-full">
         <MainTitleSec
           selectedType={selectedType}
           setSelectedType={setSelectedType}
