80658853b90ea6166c0598f129c7db454d5c1b02
dean-teamwise <dean@teamwise.im>
2025-06-18 11:15:08 +0900

    design: production 페이지-납품 탭

--- frontend/src/app/(with-layout)/production/delivery/index.tsx
+++ frontend/src/app/(with-layout)/production/delivery/index.tsx
@@ -1,5 +1,35 @@
+import MiniBtn from "@/ui/mini-btn";
+import DeliveryTableHeader from "./delivery-table-header";
+import DeliveryTableItem from "./delivery-table-item";
+
 const Delivery = () => {
-  return <div>Delivery</div>;
+  return (
+    <div className="flex flex-col px-10 pb-9">
+      <div className="flex justify-between py-4">
+        <div className="flex gap-2">
+          <MiniBtn text="납품표 일괄 출력" borderColor="border-[#eeeeee]" />
+          <MiniBtn text="납품표 출력" borderColor="border-[#eeeeee]" />
+        </div>
+        <div className="flex gap-2">
+          <MiniBtn text="거래명세서 생성" borderColor="border-[#eeeeee]" />
+          <MiniBtn text="세금계산서 생성" borderColor="border-[#eeeeee]" />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <DeliveryTableHeader />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+        <DeliveryTableItem />
+      </div>
+    </div>
+  );
 };
 
 export default Delivery;
