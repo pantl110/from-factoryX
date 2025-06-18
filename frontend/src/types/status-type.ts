f4e03abe3e291db4afb1a382d04ed9b2359a2a23
dean-teamwise <dean@teamwise.im>
2025-06-18 14:03:42 +0900

    fix: 충돌 수정

--- frontend/src/types/status-type.ts
+++ frontend/src/types/status-type.ts
@@ -12,6 +12,9 @@ export type StatusType = "충분" | "부족";
 // 설비 가동 상태 타입
 export type OperationStatusType = "가동 대기" | "가동 중" | "가동 완료";
 
+// 재고 상태 타입
+export type InventoryStatusType = "충분" | "부족";
+
 // 상태별 색상 모델 인터페이스
 export interface StatusColorModel {
   bgColor: string;
@@ -44,4 +47,13 @@ export const operationStatusColorMap: Record<
   "가동 대기": { textColor: "text-dg", bgColor: "bg-bg" },
   "가동 중": { textColor: "text-purple", bgColor: "bg-purple-8" },
   "가동 완료": { textColor: "text-primary", bgColor: "bg-primary-8" },
+};
+
+// 재고 상태별 색상 매핑
+export const inventoryStatusColorMap: Record<
+  InventoryStatusType,
+  StatusColorModel
+> = {
+  충분: { textColor: "text-primary", bgColor: "bg-primary-8" },
+  부족: { textColor: "text-red", bgColor: "bg-red-8" },
 };
