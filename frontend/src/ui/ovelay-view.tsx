9bdd19808186118ca1685e52ae8a69b48f947220
june <june@teamwise.im>
2025-06-19 12:36:59 +0900

    feat: email view 오버레이 화면 기능 구현, 디자인 수정 반영

---
+++ frontend/src/ui/ovelay-view.tsx
@@ -0,0 +1,22 @@
+interface OverlayViewProps {
+  children: React.ReactNode;
+  onClose?: () => void;
+}
+
+const OverlayView = ({ children, onClose }: OverlayViewProps) => {
+  return (
+    <div
+      className="bg-black/50 w-full h-full fixed top-0 left-0 z-50 flex justify-center items-center px-15 py-9.25"
+      onClick={onClose}
+    >
+      <div
+        className="bg-white w-full h-full overflow-y-auto"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default OverlayView;
