75b7e99204ff0a0b1ba99080e0339c49f393c3c6
june <june@teamwise.im>
2025-06-22 12:16:31 +0900

    feat: tax페이지 탭스, 문서 종류에 따른 판넬 나오기

--- frontend/src/app/(with-layout)/tax/main-title-sec.tsx
+++ frontend/src/app/(with-layout)/tax/main-title-sec.tsx
@@ -1,13 +1,34 @@
-const MainTitleSec = () => {
+import { TaxDocumentType } from "@/types/status-type";
+
+interface MainTitleSecProps {
+  selectedTaxType: TaxDocumentType | "전체";
+  setSelectedTaxType: (type: TaxDocumentType | "전체") => void;
+}
+
+const MainTitleSec = ({
+  selectedTaxType,
+  setSelectedTaxType,
+}: MainTitleSecProps) => {
+  const tabs: (TaxDocumentType | "전체")[] = ["전체", "매출", "매입"];
+
   return (
     <div className="flex flex-col gap-8 pt-10 pr-10 pl-10">
       <div className="flex items-center justify-between">
         <div className="Heading-1 text-dg">세무/회계</div>
       </div>
+
       <div className="flex gap-4 items-center Heading-3">
-        <h3 className="text-dg">전체</h3>
-        <h3 className="text-gr">매출</h3>
-        <h3 className="text-gr">매입</h3>
+        {tabs.map((tab) => (
+          <button
+            key={tab}
+            className={`${
+              selectedTaxType === tab ? "text-dg" : "text-gr"
+            } cursor-pointer`}
+            onClick={() => setSelectedTaxType(tab)}
+          >
+            {tab}
+          </button>
+        ))}
       </div>
     </div>
   );
--- frontend/src/ui/panel.tsx
+++ frontend/src/ui/panel.tsx
@@ -4,11 +4,10 @@ import { useEffect } from "react";
 interface PanelProps {
   children: React.ReactNode;
   title: string;
-  isPanelOpen: boolean;
   onClose: () => void;
 }
 
-const Panel = ({ children, title, isPanelOpen, onClose }: PanelProps) => {
+const Panel = ({ children, title, onClose }: PanelProps) => {
   useEffect(() => {
     const originalStyle = document.body.style.overflow;
     document.body.style.overflow = "hidden";
@@ -20,22 +19,18 @@ const Panel = ({ children, title, isPanelOpen, onClose }: PanelProps) => {
 
   return (
     <>
-      {isPanelOpen && (
-        <div
-          className="fixed inset-0 bg-bl/50 transition-opacity duration-300"
-          role="button"
-          tabIndex={0}
-          onClick={onClose}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" || e.key === " ") onClose();
-          }}
-        />
-      )}
+      <div
+        className="fixed inset-0 bg-bl/50 transition-opacity duration-300"
+        role="button"
+        tabIndex={0}
+        onClick={onClose}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" || e.key === " ") onClose();
+        }}
+      />
 
       <div
-        className={`fixed top-0 right-0 h-full transition-transform duration-300 ease-in-out ${
-          isPanelOpen ? "translate-x-0" : "translate-x-full"
-        }`}
+        className={`fixed top-0 right-0 h-full transition-transform duration-300 ease-in-out translate-x-0`}
       >
         <div className="w-[1000px] bg-white h-full flex flex-col gap-6 px-10 py-5">
           <div className="flex gap-2 items-center border-b border-[#eeeeee] pb-2 sticky top-0 bg-white z-10">
