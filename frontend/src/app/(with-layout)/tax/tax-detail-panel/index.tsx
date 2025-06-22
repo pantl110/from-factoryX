5d37349924c1ee0d4f0222ad782fe4dddfb5a8f7
june <june@teamwise.im>
2025-06-22 12:16:31 +0900

    feat: tax페이지 탭스, 문서 종류에 따른 판넬 나오기

--- frontend/src/app/(with-layout)/tax/tax-detail-panel/index.tsx
+++ frontend/src/app/(with-layout)/tax/tax-detail-panel/index.tsx
@@ -1,31 +1,16 @@
-import ClientInfoTable from "./client-info-table";
-import PriceInfo from "@/ui/price-info";
-import DetailSalesHeader from "@/app/(with-layout)/tax/tax-detail-panel/detail-sales-header";
-import DetailSalesItem from "@/app/(with-layout)/tax/tax-detail-panel/detail-sales-item";
+import { TaxDataModel } from "@/mocks/tax-data";
 import Panel from "@/ui/panel";
+import TaxDocumentView from "@/app/(with-layout)/document/tax-document-view";
 
 interface TaxDetailPanelProps {
-  isPanelOpen: boolean;
+  item: TaxDataModel;
   onClose: () => void;
 }
 
-const TaxDetailPanel = ({ onClose, isPanelOpen }: TaxDetailPanelProps) => {
+const TaxDetailPanel = ({ item, onClose }: TaxDetailPanelProps) => {
   return (
-    <Panel title="세무/회계" isPanelOpen={isPanelOpen} onClose={onClose}>
-      <div>
-        <ClientInfoTable />
-        <div>
-          <h3 className="Heading-3 text-dg mb-3">주문 품목 정보</h3>
-          <PriceInfo />{" "}
-        </div>
-      </div>
-      <div>
-        <DetailSalesHeader />
-        <DetailSalesItem />
-        <DetailSalesItem />
-        <DetailSalesItem />
-        <DetailSalesItem />
-      </div>
+    <Panel title="세무/회계" onClose={onClose}>
+      <TaxDocumentView taxType={item.taxType} />
     </Panel>
   );
 };
