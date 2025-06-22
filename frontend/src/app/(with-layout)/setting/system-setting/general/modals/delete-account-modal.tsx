8d9acefab67fc1a7f1231c5308892d9d65e64565
june <june@teamwise.im>
2025-06-22 14:22:54 +0900

    design: setting페이지 시스템리, 운영자조회자, 권한설정 수정된 디자인 반영

---
+++ frontend/src/app/(with-layout)/setting/system-setting/general/modals/delete-account-modal.tsx
@@ -0,0 +1,34 @@
+import MiniBtn from "@/ui/mini-btn";
+import Modal from "@/ui/modal";
+
+interface DeleteAccountModalProps {
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const DeleteAccountModal = ({
+  onClose,
+  onConfirm,
+}: DeleteAccountModalProps) => {
+  return (
+    <Modal
+      title="계정을 정말 삭제하시겠어요?"
+      subtitle="계정을 삭제하면 모든 정보가 사라지며, 복구할 수 없습니다."
+      onClose={onClose}
+      width="w-[420px]"
+      sm={true}
+    >
+      <div className="flex justify-end gap-[5px] mt-4">
+        <MiniBtn text="취소하기" textColor="text-sv" onClick={onClose} />
+        <MiniBtn
+          text="삭제하기"
+          bgColor="bg-red-8"
+          textColor="text-red"
+          onClick={onConfirm}
+        />
+      </div>
+    </Modal>
+  );
+};
+
+export default DeleteAccountModal;
