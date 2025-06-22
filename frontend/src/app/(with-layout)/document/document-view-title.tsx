7012dd40b1e56b5481e9cef68c88e99daa96492d
june <june@teamwise.im>
2025-06-22 21:09:17 +0900

    design: document 생산지시서

--- frontend/src/app/(with-layout)/document/document-view-title.tsx
+++ frontend/src/app/(with-layout)/document/document-view-title.tsx
@@ -1,7 +1,7 @@
 interface DocumentViewTitleProps {
   title: string;
-  dateLabel: string;
-  date: string;
+  dateLabel?: string;
+  date?: string;
 }
 
 const DocumentViewTitle = ({
@@ -13,8 +13,8 @@ const DocumentViewTitle = ({
     <div className="flex justify-between">
       <h2 className="Heading-2">{title}</h2>
       <div className="flex items-center gap-3 px-3 Heading-5 text-sv">
-        <h5>{dateLabel}</h5>
-        <h5>{date}</h5>
+        {dateLabel && <h5>{dateLabel}</h5>}
+        {date && <h5>{date}</h5>}
       </div>
     </div>
   );
