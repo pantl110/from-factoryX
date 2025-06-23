c6a5e4415041eb19cfee7e1720cb82d887cab1fe
june <june@teamwise.im>
2025-06-23 22:30:05 +0900

    feat: production/delivery 모달

--- frontend/src/app/(with-layout)/production/production-monitor/return/index.tsx
+++ frontend/src/app/(with-layout)/production/production-monitor/return/index.tsx
@@ -1,40 +1,20 @@
-import InfoLabelValue from "@/ui/info-label-value";
-import PriceInfo from "@/ui/price-info";
 import ReturnTableHeader from "./return-table-header";
 import ReturnTableItem from "./return-table-item";
+import ReturnInfo from "./return-info";
+import { returnData } from "@/mocks/return-data";
 
 const ReturnSection = () => {
   return (
     <div className="flex-1 h-full">
-      <div className="flex flex-col gap-3 pt-5 pb-10">
-        <h3 className="Heading-3 text-dg">반품 정보</h3>
-        <div>
-          <div className="border-t border-b border-[#eeeeee]">
-            <InfoLabelValue label="반품 일자" value="2025-06-06" />
-          </div>
-          <div className="border-b border-[#eeeeee]">
-            <InfoLabelValue label="반품 수량" value="300" />
-          </div>
-          <div className="border-b border-[#eeeeee]">
-            <InfoLabelValue
-              label="생산해야되는 수량 -> 생산계획에 들어가야함"
-              value="200"
-            />
-          </div>
-          <div className="border-b border-[#eeeeee]">
-            <InfoLabelValue
-              label="재고에서 뺄 수량 -> 재고랑 연결"
-              value="100/100"
-            />
-          </div>
-        </div>
-        <PriceInfo />
-        <div>
-          <ReturnTableHeader />
-          <ReturnTableItem />
-          <ReturnTableItem />
-          <ReturnTableItem />
-        </div>
+      {returnData.map((item) => (
+        <ReturnInfo key={item.id} returnData={item} />
+      ))}
+
+      <div className="hidden">
+        <ReturnTableHeader />
+        <ReturnTableItem />
+        <ReturnTableItem />
+        <ReturnTableItem />
       </div>
     </div>
   );
