d20c08ea41c5b5630dae8de95be77e3cfe2f98f5
june <june@teamwise.im>
2025-06-29 06:34:11 +0900

    production에서 sidebar 숨김

--- frontend/src/app/(with-layout)/layout.tsx
+++ frontend/src/app/(with-layout)/layout.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import TopBar from "@/components/top-bar";
 import SideBar from "@/components/side-bar";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const WithLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+  const isProductionPage = pathname.startsWith("/production/"); // production 페이지인지 확인
+
+  // 사이드바 상태를 업데이트하는 함수
+  const handleSidebarVisibilityChange = (visible: boolean) => {
+    setIsSidebarVisible(visible);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
-      <SideBar />
-      <div className="ml-64 flex flex-col min-h-screen">
+      <SideBar onVisibilityChange={handleSidebarVisibilityChange} />
+      <div
+        className={`flex flex-col min-h-screen transition-all duration-300 ease-in-out ${
+          isProductionPage && !isSidebarVisible ? "ml-0" : "ml-64"
+        }`}
+      >
         <div className="max-w-[1400px] min-w-[1000px] mx-auto w-full">
           <TopBar />
         </div>
