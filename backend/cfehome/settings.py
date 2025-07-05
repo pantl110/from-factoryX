4cdfa85caf680f0b7f1d7d363ffa4598faf28fd3
updaun <updowney@daum.net>
2025-07-06 01:36:17 +0900

    feat: user api setting

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -125,7 +125,12 @@ WSGI_APPLICATION = "cfehome.wsgi.application"
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=300)
 DATABASE_URL = config("DATABASE_URL", default=None)
-DATABASES = {}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
 if DATABASE_URL is not None:
     import dj_database_url
 
