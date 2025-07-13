2a0da8a1956ac352f9e16017b410f8a87dd60250
ethan <ethan@teamwise.im>
2025-07-13 12:15:58 +0900

    feat: update location api

--- backend/document/schemas/outbound.py
+++ backend/document/schemas/outbound.py
@@ -5,7 +5,7 @@ class QuotationProductOut(Schema):
     id: int
     quotation: int
     product: int
-    quantity: int
-    unit_price: int
+    quantity: Optional[int] = None
+    unit_price: Optional[int] = None
     is_delivery: Optional[bool] = None
     delivery_date: Optional[str] = None
