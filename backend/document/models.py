a4d14206879ad8955b9104ba187056305e45400c
ethan <ethan@teamwise.im>
2025-08-08 16:10:55 +0900

    feat: update document model

--- backend/document/models.py
+++ backend/document/models.py
@@ -60,6 +60,10 @@ class QuotationProduct(BaseModel):
         blank=True,
         help_text="납품 일자",
     )
+    is_refund = models.BooleanField(
+        default=False,
+        help_text="반품 여부",
+    )
 
 
 class WorkInstruction(BaseModel):
