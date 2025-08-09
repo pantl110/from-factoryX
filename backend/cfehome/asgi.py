48e62a5215f140dd7feed7e1c4a766abc3ebc648
updaun <updowney@daum.net>
2025-08-10 02:51:09 +0900

    refactor: simplify ASGI configuration and remove unused routing functions

--- backend/cfehome/asgi.py
+++ backend/cfehome/asgi.py
@@ -1,18 +1,21 @@
-"""
-ASGI config for cfehome project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
-# from django.core.asgi import get_asgi_application
-from cfehome.routing import get_application
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from websocket.middlewares import CustomAuthMiddleware
+from notification.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
 
-# application = get_asgi_application()
-application = get_application()
+# 1. Django 앱 먼저 초기화
+django_asgi_app = get_asgi_application()
+
+# 2. ProtocolTypeRouter 구성
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            CustomAuthMiddleware(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
