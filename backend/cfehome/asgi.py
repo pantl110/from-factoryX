21c82a5fdf634a8bd300dd0938c7377196b95a0d
updaun <updowney@daum.net>
2025-08-10 02:58:27 +0900

    refactor: reorder imports and update comments in ASGI configuration

--- backend/cfehome/asgi.py
+++ backend/cfehome/asgi.py
@@ -2,15 +2,16 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from websocket.middlewares import CustomAuthMiddleware
-from notification.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
 
-# 1. Django 앱 먼저 초기화
+# 먼저 Django 앱 초기화
 django_asgi_app = get_asgi_application()
 
-# 2. ProtocolTypeRouter 구성
+# 앱 초기화 뒤에 미들웨어, URL 패턴 import
+from websocket.middlewares import CustomAuthMiddleware
+from notification.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
