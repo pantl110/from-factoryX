d6548ac3fb2194d91b3d52c967b8070e675927df
updaun <updowney@daum.net>
2025-08-20 23:22:08 +0900

    feat: enhance tax invoice publishing with websocket notifications and improve factory member validation

--- backend/tax/utils.py
+++ backend/tax/utils.py
@@ -6,7 +6,7 @@ async def get_tax_service_by_id(tax_service_id: int):
     try:
         tax_service = (
             await NationalTaxService.objects.select_related("client", "factory", "user")
-            .prefetch_related("product")
+            .prefetch_related("product", "projects")
             .aget(id=tax_service_id)
         )
         return tax_service
