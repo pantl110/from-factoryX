88b910fc26fbe45957c92c7d7a4464a79653e36c
june <june@juneui-MacBookAir.local>
2025-09-04 13:13:19 +0900

    견적서 이메일 전송 용량 제한 30MB로 증가

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -345,4 +345,4 @@ TOSS_PAYMENTS_BASE_URL = "https://api.tosspayments.com"
 SCHEDULING_SECRET_KEY = config("SCHEDULING_SECRET_KEY", default=None)
 
 # send-email with attachment
-DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024  # 10MB
+DATA_UPLOAD_MAX_MEMORY_SIZE = 30 * 1024 * 1024  # 30MB
