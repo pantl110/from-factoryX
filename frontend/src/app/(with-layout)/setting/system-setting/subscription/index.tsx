ad87e8e0b48bdee57a4c1a65bf303ef94d0d54e0
june <june@juneui-MacBookAir.local>
2025-09-15 15:04:23 +0900

    결제 카드 설정 정보

--- frontend/src/app/(with-layout)/setting/system-setting/subscription/index.tsx
+++ frontend/src/app/(with-layout)/setting/system-setting/subscription/index.tsx
@@ -134,17 +134,25 @@ const Subscription = () => {
         </div>
 
         <div className="flex items-center justify-between h-18 py-4 px-6 border border-lg rounded-xl">
-          <h4 className="Heading-4">
-            {subscriptionStatus?.current_payment?.card_company}{' '}
-            {subscriptionStatus?.current_payment?.card_number}
-          </h4>
-          <MiniBtn
-            text="삭제"
-            textColor="text-red"
-            bgColor="bg-red-8"
-            hoverColor="hover:bg-red-hover"
-            onClick={() => setIsCardDeleteModalOpen(true)}
-          />
+          {factory?.billing_key ? (
+            <>
+              <h4 className="Heading-4">
+                {subscriptionStatus?.current_payment?.card_company}{' '}
+                {subscriptionStatus?.current_payment?.card_number}
+              </h4>
+              <MiniBtn
+                text="삭제"
+                textColor="text-red"
+                bgColor="bg-red-8"
+                hoverColor="hover:bg-red-hover"
+                onClick={() => setIsCardDeleteModalOpen(true)}
+              />
+            </>
+          ) : (
+            <p className="Me_Body-2 text-gr text-center w-full">
+              등록된 결제 카드가 없습니다.
+            </p>
+          )}
         </div>
       </div>
 
