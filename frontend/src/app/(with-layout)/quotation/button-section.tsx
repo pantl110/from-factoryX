c642abbfbd060926b130ae13f84b07c38cc8b12a
june <june@juneui-MacBookAir.local>
2025-09-17 23:26:32 +0900

    이메일 전송 시 공장명 누락 → 버튼 disabled & 툴팁 생성

--- frontend/src/app/(with-layout)/quotation/button-section.tsx
+++ frontend/src/app/(with-layout)/quotation/button-section.tsx
@@ -4,9 +4,10 @@ import MiniBtn from '@/ui/mini-btn';
 import { ArrowRight } from '@phosphor-icons/react/dist/ssr';
 import { useRouter } from 'next/navigation';
 import Tooltip from '@/ui/tooltip';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useMemberStore from '@/store/member-store';
 import TaxDetailPanel from '../tax/tax-detail-panel';
+import { useGetFactory } from '@/hooks';
 
 interface ButtonSectionProps {
   setIsTaxCreatePanelOpen: (open: boolean) => void;
@@ -40,9 +41,22 @@ const ButtonSection = ({
   const router = useRouter();
   const role = useMemberStore((state) => state.role);
   const isViewer = role === 'viewer';
+  const factoryId = useMemberStore((state) => state.factoryId);
 
-  const [showTooltip, setShowTooltip] = useState(false);
+  const { getFactory, factory } = useGetFactory();
+
+  const [showTaxTooltip, setShowTaxTooltip] = useState(false);
+  const [showFactoryNameTooltip, setShowFactoryNameTooltip] = useState(false);
   const [isTaxDetailPanelOpen, setIsTaxDetailPanelOpen] = useState(false);
+
+  useEffect(() => {
+    if (factoryId) {
+      getFactory(factoryId);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [factoryId]);
+  const hasFactoryName = factory?.name;
+
   return (
     <>
       <div className="flex gap-1">
@@ -50,18 +64,16 @@ const ButtonSection = ({
           className="relative"
           onMouseEnter={() => {
             if (!isOrderStatus && !taxId) {
-              setShowTooltip(true);
+              setShowTaxTooltip(true);
             }
           }}
           onMouseLeave={() => {
-            setShowTooltip(false);
+            setShowTaxTooltip(false);
           }}
         >
           <MiniBtn
             text={taxId ? '세금계산서 보기' : '세금계산서 생성'}
-            textColor="text-dg"
-            borderColor="border-lg"
-            hoverColor="hover:bg-bg"
+            variant="whiteOutline"
             disabled={
               !taxId &&
               (!isFormFilled ||
@@ -77,7 +89,7 @@ const ButtonSection = ({
               }
             }}
           />
-          {showTooltip && !taxId && !isOrderStatus && (
+          {showTaxTooltip && !taxId && !isOrderStatus && (
             <div className="absolute z-50 top-12 left-0 w-[350px]">
               <Tooltip
                 text={'세금계산서는 주문을 확정한 후에 생성할 수 있어요.'}
@@ -87,31 +99,46 @@ const ButtonSection = ({
             </div>
           )}
         </div>
-        <MiniBtn
-          text="출력"
-          textColor="text-dg"
-          borderColor="border-lg"
-          onClick={onPrintClick}
-          hoverColor="hover:bg-bg"
-        />
-        <MiniBtn
-          text="이메일 전송"
-          textColor="text-dg"
-          borderColor="border-lg"
-          onClick={onEmailClick}
-          hoverColor="hover:bg-bg"
-          disabled={!isFormFilled || !hasQuotationProducts || isViewer}
-        />
+        <MiniBtn text="출력" variant="whiteOutline" onClick={onPrintClick} />
+        <div
+          className="relative"
+          onMouseEnter={() => {
+            setShowFactoryNameTooltip(true);
+          }}
+          onMouseLeave={() => {
+            setShowFactoryNameTooltip(false);
+          }}
+        >
+          <MiniBtn
+            text="이메일 전송"
+            variant="whiteOutline"
+            onClick={onEmailClick}
+            disabled={
+              !isFormFilled ||
+              !hasQuotationProducts ||
+              isViewer ||
+              !hasFactoryName
+            }
+          />
+          {!hasFactoryName && showFactoryNameTooltip && (
+            <div className="absolute z-50 top-12 left-0 w-[250px]">
+              <Tooltip
+                text={'설정에서 회사명을 입력해 주세요.'}
+                color="white"
+                position="left"
+              />
+            </div>
+          )}
+        </div>
+
         {isOrderStatus ? (
           <>
             <MiniBtn
               text="생산 시작"
-              textColor="text-wh"
-              bgColor="bg-primary"
+              variant="primary"
               icon={ArrowRight}
               iconPosition="right"
               onClick={onStartProductionClick}
-              hoverColor="hover:bg-primary-hover"
               disabled={!isFormFilled || !hasQuotationProducts || isViewer}
             />
           </>
@@ -119,8 +146,7 @@ const ButtonSection = ({
           <>
             <MiniBtn
               text="임시 저장"
-              textColor="text-primary"
-              bgColor="bg-primary-8"
+              variant="secondary"
               onClick={async () => {
                 try {
                   const isSuccess = await onSaveDraft?.(false);
@@ -131,13 +157,11 @@ const ButtonSection = ({
                   // 에러가 발생하면 페이지 이동하지 않음
                 }
               }}
-              hoverColor="hover:bg-secondary-hover"
               disabled={!isDirty || isViewer || isSaveDraftLoading}
             />
             <MiniBtn
               text="주문 확정"
-              textColor="text-wh"
-              bgColor="bg-primary"
+              variant="primary"
               onClick={async () => {
                 try {
                   const isSuccess = await onSaveDraft?.(true);
@@ -149,7 +173,6 @@ const ButtonSection = ({
                   // 에러가 발생하면 페이지 이동하지 않음
                 }
               }}
-              hoverColor="hover:bg-primary-hover"
               disabled={
                 !isFormFilled ||
                 !hasQuotationProducts ||
