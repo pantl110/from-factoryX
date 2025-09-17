aa616d4f52b5662b0164c441770ba1e1f59c939d
june <june@juneui-MacBookAir.local>
2025-09-17 18:11:36 +0900

    feat(workinstruction): include additional plan annotations in work-instruction detail

--- backend/document/api_workinstruction.py
+++ backend/document/api_workinstruction.py
@@ -91,10 +91,12 @@ async def get_work_instruction(request, work_instruction_id: int):
                     .prefetch_related("project__quotations__client")
                     .annotate(
                         client_name=F("project__quotations__client__name"),
+                        factory_name=F("factory__name"),
                         product_name=F("product__product__name"),
                         product_code=F("product__product__code"),
                         product_unit=F("product__product__unit"),
                         product_spec=F("product__product__spec"),
+                        product_note=F("product__product__note"),
                     ),
                 )
             ).get(id=work_instruction_id, factory_id=factory_id)
