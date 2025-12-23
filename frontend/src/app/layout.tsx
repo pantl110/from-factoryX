a2b93d7e9c1ed34458bb072089619dab6f067867
june <june@conscience.partners>
2025-12-23 16:14:33 +0900

    refactor: Update metadata title and description in layout.tsx for consistency

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,8 +29,8 @@ const pretendard = localFont({
 });
 
 export const metadata = {
-  title: 'Factory_X',
-  description: 'Factory_X',
+  title: 'FactoryX',
+  description: 'FactoryX',
   icons: {
     icon: '/favicon.svg',
   },
